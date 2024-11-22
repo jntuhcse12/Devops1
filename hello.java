d2d04a532a0e8a80eb8b2bab545be6ca627feb39
cejcse12 <cejcse12@gmail.com>
2024-11-22 12:05:58 +0530

    Initial commit

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
  
 // Your First Program
 
-class HelloWorld {
+class hello{
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("hello"); 
     }
 }
